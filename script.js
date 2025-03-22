7d5f09939902417da21cb9fc5898d05674c10e59
MuhitRonon20 <iamronon17official@gmail.com>
2025-03-22 21:27:31 +0600

    final commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    alert("Welcome to Gaming Accessories Store! Time to upgrade your setup!");
+}
+
+function buyNow(productName) {
+    alert("Thank you for choosing the " + productName + "! Your order is being processed.");
+}
+function sendMessage() {
+    
+    alert("Your message has been sent!");
+
+
+}
